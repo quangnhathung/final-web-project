f9973b32f849abb991fe1356f9f391494e63d8a5
QuangNhatHung <quangnhathung@gmail.com>
2025-05-20 17:27:43 +0700

    homePage: update ui

--- src/script/homePage/LoadTops.js
+++ src/script/homePage/LoadTops.js
@@ -1,10 +1,10 @@
 const Tops = [
-    {img: "../../../assets/homePage/TOPS/0823f2bf88204b8d829c6c67721a0922.jpg" , name:"ZIGZAG HOODED KHAKI JACKET™", button: "# ", price:"375.000đ"},
-    {img: "../../../assets/homePage/TOPS/h60x6UW.png" , name:"KAKI FACE JACKET  HOODED  SHIRT™", button: "# ", price:"375.000đ"},
-    {img: "../../../assets/homePage/TOPS/kKXp1W9.jpeg" , name:"SHEEPSMAN FACE KAKI JACKET", button: "# ", price:"251.000đ"},
-    {img: "../../../assets/homePage/TOPS/pUcOAao.png" , name:"BLACKSHEEPS  JACKET POLO SS TEE", button: "# ", price:"375.000đ"},
-    {img: "../../../assets/homePage/TOPS/Vj3rWMX.jpeg" , name:"BIG LOGO FLANNEL SLEEVE TEE SHIRT™", button: "# ", price:"175.000đ"},
-    {img: "../../../assets/homePage/TOPS/vqMnJ23.png" , name:"BLACKSQUARE LONG SLEEVE TEE", button: "# ", price:"99.000đ"}
+    {img: "../../../assets/homePage/TOPS/0823f2bf88204b8d829c6c67721a0922.jpg" , name:"ZIGZAG HOODED KHAKI JACKET™", button: "# ", price:"375.000đ",sale:"450.000d"},
+    {img: "../../../assets/homePage/TOPS/h60x6UW.png" , name:"KAKI FACE JACKET  HOODED  SHIRT™", button: "# ", price:"375.000đ",sale:"430.000d"},
+    {img: "../../../assets/homePage/TOPS/kKXp1W9.jpeg" , name:"SHEEPSMAN FACE KAKI JACKET", button: "# ", price:"251.000đ",sale:"285.000d"},
+    {img: "../../../assets/homePage/TOPS/pUcOAao.png" , name:"BLACKSHEEPS  JACKET POLO SS TEE", button: "# ", price:"375.000đ",sale:"400.000d"},
+    {img: "../../../assets/homePage/TOPS/Vj3rWMX.jpeg" , name:"BIG LOGO FLANNEL SLEEVE TEE SHIRT™", button: "# ", price:"175.000đ",sale:"250.000d"},
+    {img: "../../../assets/homePage/TOPS/vqMnJ23.png" , name:"BLACKSQUARE LONG SLEEVE TEE", button: "# ", price:"99.000đ",sale:"150.000d"}
 ]
 
 const contentTop = document.getElementById("content-top");
@@ -19,7 +19,7 @@ Tops.forEach(product => {
             <img src="${product.img}" class="card-img-top" alt="${product.name}">
             <div class="card-body d-flex flex-column">
                 <h5 class="card-title">${product.name}</h5>
-                <p class="card-text text-danger fs-4 fw-bold">${product.price}</p>
+                <p class="card-text text-danger fs-4 fw-bold">${product.price} <s style="font-size: 15px;color:rgb(127, 130, 130);">${product.sale}</s></p>
                 <input hidden class="cart-toggle" id="cart-toggle" type="checkbox" />
                 <label class="cart-button text-center">
                     <a href="#" style = "color:white;text-decoration: none;">
