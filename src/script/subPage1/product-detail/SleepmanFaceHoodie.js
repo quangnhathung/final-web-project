ff995efca27732c1751180647c5e40f59642db83
hkhanh05 <huukhanhnguyen10012005@gmail.com>
2025-05-30 22:40:03 +0700

    create some detail product
    create some detail product

---
+++ src/script/subPage1/product-detail/SleepmanFaceHoodie.js
@@ -0,0 +1,54 @@
+function increaseQty() {
+      let qtyInput = document.getElementById("quantity");
+      let value = parseInt(qtyInput.value);
+      qtyInput.value = value + 1;
+}
+  
+function decreaseQty() {
+      let qtyInput = document.getElementById("quantity");
+      let value = parseInt(qtyInput.value);
+      if (value > 1) {
+        qtyInput.value = value - 1;
+      }
+}
+
+const imageData = {
+    yellow: [
+        '../../../assets/public/UZ4JuK5.png',
+        '../../../assets/thumbnail/SleepmanFaceHoodie_yellow1.png'
+    ],
+};
+
+function updateImages(color) {
+      const images = imageData[color];
+      if (!images) return;
+
+      const mainImage = document.getElementById('mainImage');
+      mainImage.src = images[0];
+
+      const container = document.getElementById('thumbnailContainer');
+      container.innerHTML = '';
+
+      images.forEach((src, index) => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.className = 'img-thumbnail thumb-img img-bordered';
+        if (index === 0) img.classList.add('selected');
+
+        img.onclick = () => {
+          mainImage.src = src;
+          document.querySelectorAll('.thumb-img').forEach(t => t.classList.remove('selected'));
+          img.classList.add('selected');
+        };
+
+        container.appendChild(img);
+      });
+}
+
+document.querySelectorAll('input[name="color"]').forEach(input => {
+      input.addEventListener('change', function () {
+        updateImages(this.id);
+      });
+});
+
+window.onload = () => updateImages('yellow');
